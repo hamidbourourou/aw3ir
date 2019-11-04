a71f1e304205a3b69c6564f0a18f8f093f685820
hamidbourourou <56534925+hamidbourourou@users.noreply.github.com>
2019-11-04 11:51:57 +0100

    Update formulaire.js

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,17 +1,17 @@
 function validation() {
     if (document.getElementById("nom").value.length < 5) {
-        return document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
+         document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
       } 
       else if (document.getElementById("prenom").value.length < 5) {
-        return document.getElementById("error").innerHTML = "La saisie du prenom est obligatoire";
+         document.getElementById("error").innerHTML = "La saisie du prenom est obligatoire";
       } 
       else if (document.getElementById("Adress").value.length < 5) {
-        return document.getElementById("error").innerHTML = "La saisie de l'Adresse postale est obligatoire";
+         document.getElementById("error").innerHTML = "La saisie de l'Adresse postale est obligatoire";
       } 
       else if (document.getElementById("mail").value.length < 5) {
-        return document.getElementById("error").innerHTML = "La saisie de l'email est obligatoire";
+         document.getElementById("error").innerHTML = "La saisie de l'email est obligatoire";
       } 
       else {
-        return document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;;
+         document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;;
         }            
   }
