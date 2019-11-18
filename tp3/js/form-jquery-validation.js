6b314d79a7c65649ef649b23c35cf33a24708eaa
hamidbourourou <56534925+hamidbourourou@users.noreply.github.com>
2019-11-18 11:51:39 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,19 +1,37 @@
-jQuery(document).ready(function () {
-    jQuery("#monFormulaire").validate({
+$(document).ready(function () {
+    $("#mainForm").validate({
         rules: {
             "nom": {
                 "required": true,
-                "minlength": 2,
+                "minlength": 3,
                 "maxlength": 60000
             },
             "email": {
+                "required":true,
                 "email": true,
                 "maxlength": 255
             },
             "prenom": {
                 "required": true,
-                "minlength":2
+                "minlength":3
+            },
+            "adresse":{
+                "required": true,
+                "minlength":8
+
+            },
+            "date":{
+
+
             }
+        },
+        messages: {
+            nom: <div class="alert alert-primary" role="alert">
+                This is a primary alert—check it out!
+</div>,
+            prenom: "Veuillez fournir un prenom d'au moins trois lettres",
+            email: "L'email est incorrect",
+            adresse: " Veuillez fournir une adresse valide"
         }
     })
 });
