8dd84ad49beeb70b1c472a5f9dffbc4b85a477d2
hamidbourourou <56534925+hamidbourourou@users.noreply.github.com>
2019-11-25 11:09:18 +0100

    Update form-jquery-validation.js

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -1,8 +1,8 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-   
 
-    $("#form1").on("submit",function(event){
+
+    $("#form1").on("submit", function (event) {
 
         event.preventDefault();
 
@@ -10,86 +10,88 @@ $( document ).ready(function() {
 
 
 
-        var y = "https://maps.googleapis.com/maps/api/staticmap?markers="+$("#adresse").val()+"&zoom=12&size=200x150&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"
+        var y = "https://maps.googleapis.com/maps/api/staticmap?markers=" + $("#adresse").val() + "&zoom=12&size=200x150&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"
 
-        var x = 0 ;
+        var x = 0;
 
 
 
-        if ($("#name").val()=="" ) {
+        if ($("#name").val() == "") {
 
             $(".hello").text("Veuillez saisir votre Name");
 
             $('#myModal').modal('show');
 
-            x = x + 1 ; 
+            x = x + 1;
 
         }
 
-        if ($("#firstname").val()=="" ) {
+        if ($("#firstname").val() == "") {
 
             $(".hello").text("Veuillez saisir votre Firstname");
 
             $('#myModal').modal('show');
 
-            x=x+1 ;
+            x = x + 1;
 
         }
 
-        if($("#birth").val()=="" ) {
+        if ($("#birth").val() == "") {
 
             $(".hello").text("Veuillez saisir votre birthday");
 
             $('#myModal').modal('show');
 
-            x=x+1 ;
+            x = x + 1;
 
 
 
         }
 
-        if ($("#adresse").val()=="" ) {
+        if ($("#adresse").val() == "") {
 
             $(".hello").text("Veuillez saisir votre Address");
 
             $('#myModal').modal('show');
 
-            x=x+1 ; }
+            x = x + 1;
+        }
 
-        
 
-        if (x==0) {
 
-            $(".modal-title").text("Welcome" +" "+ $("#name").val()) ;
+        if (x == 0) {
 
-            
+            $(".modal-title").text("Welcome" + " " + $("#name").val());
 
-            $(".modal-body").html("Vous etes nés le"+" "+$(" #birth").val()+" "+"et vous habitez"+'<img src="'+y+'"  />');
 
-            
+
+            $(".modal-body").html("Vous etes nés le" + " " + $(" #birth").val() + " " + "et vous habitez" + '<img src="' + y + '"  />');
+
+
 
             //$(".modal-body").css('border','5px black solid') ;
 
 
 
-            $('#myModal').modal('show');}
+            $('#myModal').modal('show');
+        }
 
 
 
-        
-
-       
 
 
 
-      });
 
 
 
-     console.log( "DOM ready!" );
+    });
 
-     
 
-     
 
- });
+    console.log("DOM ready!");
+
+
+
+
+
+});
