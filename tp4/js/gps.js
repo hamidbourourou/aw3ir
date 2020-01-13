09d181e3aadeca33847b1c73eeba5046472bfe43
hamidbourourou <56534925+hamidbourourou@users.noreply.github.com>
2020-01-13 10:18:58 +0100

    Update gps.js

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -12,8 +12,9 @@ function showPosition(position) {
     var latlon = position.coords.latitude + "," + position.coords.longitude;
     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
     +latlon+"&zoom=14&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg";
-    $("#adresse").val(latlon);
+    
     $("#map").html("<img src='"+img_url+"'>");
+    $("#adresse").val(latlon);
 }
 
 // Au cas ou l'utilisateur refuse
