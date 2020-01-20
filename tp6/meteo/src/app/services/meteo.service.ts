f02662aff807b70f31107eb2517940fcf55462e5
hamidbourourou <56534925+hamidbourourou@users.noreply.github.com>
2020-01-20 14:49:57 +0100

    00

---
+++ tp6/meteo/src/app/services/meteo.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { MeteoItem } from '../meteoItem';
+
+@Injectable({ providedIn: 'root' })
+export class MeteoService {
+
+  constructor() { }
+
+
+  getMeteo(name: string): Promise<any> {
+    console.log('from service', name);
+
+    let m = new MeteoItem();
+
+    return fetch('https://api.openweathermap.org/data/2.5/weather/?q=' + name + '&units=metric&lang=fr&appid=7ad63ad479e2d4724c9f20215c1fc98e')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+
+        // test du code retour
+        // 200 = OK
+        // 404 = city not found 
+        if (json.cod === 200) {
+          return Promise.resolve(json);
+        } else {
+          m.weather = json;
+
+          console.error('Météo introuvable pour ' + name
+            + ' (' + json.message + ')');
+
+          return Promise.reject('Météo introuvable pour ' + name
+          + ' (' + json.message + ')');
+        }
+
+      });
+
+  }
+}
